bb477156a7856d062edc8c72ab2fc2c36b32b059
kasiasoltysiak <126796896+kasiasoltysiak@users.noreply.github.com>
2023-05-04 13:33:57 +0200

    updated with real-life data

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-let newYorkElement = document.querySelector("#new-york");
-let newYorkDate = newYorkElement.querySelector(".date");
-newYorkDate.innerHTML = moment().format("MMMM Do YYYY");
-let newYorkTime = newYorkElement.querySelector(".time");
-newYorkTime.innerHTML = moment().format("hh:mm:ss");
-let meridianElement = document.querySelector("#meridian");
-meridianElement.innerHTML = moment().format("a");
+function updateTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDate = newYorkElement.querySelector(".date");
+  newYorkDate.innerHTML = moment()
+    .tz("America/New_York")
+    .format("MMMM Do YYYY");
+  let newYorkTime = newYorkElement.querySelector(".time");
+  newYorkTime.innerHTML = moment()
+    .tz("America/New_York")
+    .format("hh:mm:ss[<small>]A[</small>]");
+
+  let amsterdamElement = document.querySelector("#amsterdam");
+  let amsterdamDate = amsterdamElement.querySelector(".date");
+  amsterdamDate.innerHTML = moment()
+    .tz("Europe/Amsterdam")
+    .format("MMMM Do YYYY");
+  let amsterdamTime = amsterdamElement.querySelector(".time");
+  amsterdamTime.innerHTML = moment()
+    .tz("Europe/Amsterdam")
+    .format("hh:mm:ss[<small>]A[</small>]");
+
+  let klElement = document.querySelector("#kl");
+  let klDate = klElement.querySelector(".date");
+  klDate.innerHTML = moment().tz("Asia/Kuala_Lumpur").format("MMMM Do YYYY");
+  let klTime = klElement.querySelector(".time");
+  klTime.innerHTML = moment()
+    .tz("Asia/Kuala_Lumpur")
+    .format("hh:mm:ss[<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
 
 //hh:mm a
